6e14f8fb99f6c28914b20e28f0f808cc6ac2b6cb
Daniel Löfgren <dan.lofgrens@gmail.com>
2020-01-17 19:26:14 +0100

    Challange Start & misc settings

---
+++ lib/colors.dart
@@ -0,0 +1,4 @@
+import 'dart:ui';
+
+Color themeColorA = const Color(0xFFd4f4a4);
+Color themeColorB = const Color(0xFFCC45AA);
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,109 @@
 import 'package:flutter/material.dart';
+import 'package:mi_card/colors.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(LayoutChallange());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            verticalDirection: VerticalDirection.down,
+            children: <Widget>[
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.red,
+                child: Text('Container 1'),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.blue,
+                child: Text('Container 2'),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.white,
+                child: Text('Container 3'),
+              ),
+              Container(
+                width: double.infinity,
+                height: 10,
+              )
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
+
+class LayoutChallange extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            verticalDirection: VerticalDirection.down,
+            children: <Widget>[
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.red,
+                child: Text('Container 1'),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.blue,
+                child: Text('Container 2'),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.white,
+                child: Text('Container 3'),
+              ),
+              Container(
+                width: double.infinity,
+                height: 10,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/* 
+Container(
+            height: 100,
+            width: 100,
+            color: themeColorA,
+            child: Text('Hello World!'),
+          ),
+ */
